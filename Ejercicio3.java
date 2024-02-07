07acc9a38c0c6285afe4eee21d08e58e8a23560e
PaxuitoGIT <bxu@myuax.com>
2024-02-07 17:28:21 +0100

    Ejercicio 3 Mostrar resultado

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,10 +1,9 @@
-import java.sql.Array;
 import java.util.ArrayList;
 
 public class Ejercicio3 {
-    static ArrayList<Integer> MultiplosDeCinco(int begin, int end) {
+    static ArrayList<Integer> MultiplosDeCinco() {
         ArrayList<Integer> multiplos = new ArrayList<>();
-        for (int i = begin; i <= end; i++) {
+        for (int i = 1; i <= 100; i++) {
             if (i % 5 == 0) {
                 multiplos.add(i);
             }
@@ -18,5 +17,15 @@ public class Ejercicio3 {
             suma += numero;
         }
         return suma;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> MultiploCinco = MultiplosDeCinco();
+        System.out.println("Los múltiplos de 5 del 1 al 100 son:");
+        for (int multiplo : MultiploCinco) {
+            System.out.println(multiplo);
+        }
+        int suma = sumarArray(MultiploCinco);
+        System.out.println("La suma de todos los múltiplos de 5 del 1 al 100 es: " + suma);
     }
 }
