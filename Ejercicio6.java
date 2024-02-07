753f2b7b5b52d9ebd829a30e04225a2dc99d7494
PaxuitoGIT <bxu@myuax.com>
2024-02-07 19:21:08 +0100

    Ejercicio 6 verificar primo

---
+++ Ejercicio6.java
@@ -0,0 +1,13 @@
+public class Ejercicio6 {
+    static boolean esPrimo(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
