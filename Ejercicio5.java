540abc9336784ba223806fa8ccc7d0de4a5d65df
PaxuitoGIT <bxu@myuax.com>
2024-02-07 19:14:06 +0100

    Ejercicio 5 Mostrar tabla

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -1,11 +1,22 @@
 public class Ejercicio5 {
-    static int [][] generarTablasMultiplicar(int N) {
-        int[][] tablas = new int [N][10];
-        for (int i = 0; i < N; i++) {
+    static int [][] generarTablasMultiplicar() {
+        int[][] tablas = new int [10][10];
+        for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 tablas [i][j] = i * j;
             }
         }
         return tablas;
+    }
+
+    public static void main(String[] args) {
+        int [][] tablasDeMultiplicar = generarTablasMultiplicar();
+        for (int i = 0; i < tablasDeMultiplicar.length; i++) {
+            System.out.println("Tabla del " + i + ":");
+            for (int j = 0; j < tablasDeMultiplicar[i].length; j++) {
+                System.out.println(i + " * " + j + " = " + tablasDeMultiplicar[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
